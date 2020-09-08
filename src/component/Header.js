0369d495772ef64e6b626986f7153e0c37ce524a
Tomasz Sekuła <sekula.tomasz1993@gmail.com>
2020-09-08 08:37:40 +0200

    add config files

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeaderContainer = styled.div`
+    display: sticky;
+    width: 100%;
+    height: 2rem;
+`
+
+const Header = () => {
+    return null;
+}
+
+export default Header;
---
+++ src/shared/AppConfig.js
@@ -0,0 +1,11 @@
+export const typography = {
+    'H1': '96px',
+    'H2': '3.75rem',
+    'H3': '3rem',
+    'H4': '2.125rem',
+    'H5': '1.5rem',
+    'subtitle1': '1rem',
+    'subtitle2': '0.875rem',
+    'body1': '1rem',
+    'body2': '0.875rem'
+}
