ea73aff3efc8844058bf15a50961f2caf65f5810
Tomasz Sekuła <sekula.dev@gmail.com>
2020-09-17 19:34:13 +0200

    Updates

---
+++ precache-manifest.08208d241d8baaaf272c4761cc950ff7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4499388d7bcaab04b86bc7f0516d3119",
+    "url": "/-DEMO---WeatherPWA/index.html"
+  },
+  {
+    "revision": "6a84975acdff141376ac",
+    "url": "/-DEMO---WeatherPWA/static/css/main.8513b2a1.chunk.css"
+  },
+  {
+    "revision": "978004cab1d4b1d7a196",
+    "url": "/-DEMO---WeatherPWA/static/js/2.79416643.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/-DEMO---WeatherPWA/static/js/2.79416643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a84975acdff141376ac",
+    "url": "/-DEMO---WeatherPWA/static/js/main.11e9b8f0.chunk.js"
+  },
+  {
+    "revision": "a8ce7c32070922d9943c",
+    "url": "/-DEMO---WeatherPWA/static/js/runtime-main.f03ac82b.js"
+  },
+  {
+    "revision": "18ae8b6a5ffd4e0f90940179de8a3b80",
+    "url": "/-DEMO---WeatherPWA/static/media/01d.18ae8b6a.svg"
+  },
+  {
+    "revision": "abd974161640021b8ff745d1918af418",
+    "url": "/-DEMO---WeatherPWA/static/media/01n.abd97416.svg"
+  },
+  {
+    "revision": "51461db43ab7362a8c5f3dedc920e05a",
+    "url": "/-DEMO---WeatherPWA/static/media/02d.51461db4.svg"
+  },
+  {
+    "revision": "33e7fec5a07ce4c1526dd89625ad7acd",
+    "url": "/-DEMO---WeatherPWA/static/media/02n.33e7fec5.svg"
+  },
+  {
+    "revision": "6dfb3b610f1c3c27364dadadf29ef2fe",
+    "url": "/-DEMO---WeatherPWA/static/media/03d.6dfb3b61.svg"
+  },
+  {
+    "revision": "6dfb3b610f1c3c27364dadadf29ef2fe",
+    "url": "/-DEMO---WeatherPWA/static/media/03n.6dfb3b61.svg"
+  },
+  {
+    "revision": "6dfb3b610f1c3c27364dadadf29ef2fe",
+    "url": "/-DEMO---WeatherPWA/static/media/04d.6dfb3b61.svg"
+  },
+  {
+    "revision": "6dfb3b610f1c3c27364dadadf29ef2fe",
+    "url": "/-DEMO---WeatherPWA/static/media/04n.6dfb3b61.svg"
+  },
+  {
+    "revision": "2cf3bb1c2e51770601dbc9acf4da75ac",
+    "url": "/-DEMO---WeatherPWA/static/media/09d.2cf3bb1c.svg"
+  },
+  {
+    "revision": "2cf3bb1c2e51770601dbc9acf4da75ac",
+    "url": "/-DEMO---WeatherPWA/static/media/09n.2cf3bb1c.svg"
+  },
+  {
+    "revision": "3373d8224fd99914b1240e7a0a335995",
+    "url": "/-DEMO---WeatherPWA/static/media/10d.3373d822.svg"
+  },
+  {
+    "revision": "c13e538e3cccf8add32a0e174fca63c4",
+    "url": "/-DEMO---WeatherPWA/static/media/10n.c13e538e.svg"
+  },
+  {
+    "revision": "a922c3fdad02e0b1cba8aaf702b7d24c",
+    "url": "/-DEMO---WeatherPWA/static/media/11d.a922c3fd.svg"
+  },
+  {
+    "revision": "a922c3fdad02e0b1cba8aaf702b7d24c",
+    "url": "/-DEMO---WeatherPWA/static/media/11n.a922c3fd.svg"
+  },
+  {
+    "revision": "74f45bc4dfa824ddd037eb0b1f753149",
+    "url": "/-DEMO---WeatherPWA/static/media/13d.74f45bc4.svg"
+  },
+  {
+    "revision": "74f45bc4dfa824ddd037eb0b1f753149",
+    "url": "/-DEMO---WeatherPWA/static/media/13n.74f45bc4.svg"
+  },
+  {
+    "revision": "57afbca9df8e5ca1da0e0dc41e090d64",
+    "url": "/-DEMO---WeatherPWA/static/media/50d.57afbca9.svg"
+  },
+  {
+    "revision": "57afbca9df8e5ca1da0e0dc41e090d64",
+    "url": "/-DEMO---WeatherPWA/static/media/50n.57afbca9.svg"
+  },
+  {
+    "revision": "01e4b4f22af26f2618d44d1f9e3536e7",
+    "url": "/-DEMO---WeatherPWA/static/media/iconfinder_Drip_5729390.01e4b4f2.svg"
+  },
+  {
+    "revision": "7125fd31ce37dfb387163d7104362658",
+    "url": "/-DEMO---WeatherPWA/static/media/iconfinder_Foggy_Cloud_5729389.7125fd31.svg"
+  },
+  {
+    "revision": "d88b95fa163518910745d7de76b37378",
+    "url": "/-DEMO---WeatherPWA/static/media/iconfinder_Lightning_5729386.d88b95fa.svg"
+  },
+  {
+    "revision": "2be9bd0f92b28df3ed2b4ef589ddd85a",
+    "url": "/-DEMO---WeatherPWA/static/media/iconfinder_Snow_Clody_5729380.2be9bd0f.svg"
+  },
+  {
+    "revision": "85b6d51b9d624f1b9e91148da470c473",
+    "url": "/-DEMO---WeatherPWA/static/media/iconfinder_Snowing_5729381.85b6d51b.svg"
+  },
+  {
+    "revision": "e977faa6c2d9b406aa98ab80eb66e552",
+    "url": "/-DEMO---WeatherPWA/static/media/iconfinder_Windy_5729377.e977faa6.svg"
+  }
+]);
